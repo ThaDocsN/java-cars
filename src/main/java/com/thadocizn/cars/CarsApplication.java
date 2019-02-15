4d6b2785c3548aec65fb27aca1f373f25744759b
ThaDocIzN <rodan72@msn.com>
2019-02-15 12:47:46 -0600

    finish

--- src/main/java/com/thadocizn/cars/CarsApplication.java
+++ src/main/java/com/thadocizn/cars/CarsApplication.java
@@ -1,7 +1,15 @@
 package com.thadocizn.cars;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CarsApplication {
@@ -12,6 +20,30 @@ public class CarsApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarsApplication.class, args);
     }
+    @Bean
+    public TopicExchange appExchange(){
+        return new TopicExchange(EXCHANGE_NAME);
+    }
 
+    @Bean
+    public Queue appQueue(){
+        return new Queue(QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding bindQueue(){
+        return BindingBuilder.bind(appQueue()).to(appExchange()).with(QUEUE_NAME);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory){
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter());
+        return rabbitTemplate;
+    }
+
+    private Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 }
 
